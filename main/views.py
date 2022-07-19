c07fcb16bc0bb4bf015b7db379a7733eaaa41fc8
Anjali Rani <anjalirani047@gmail.com>
2022-07-19 10:27:14 +0530

    .

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.conf import settings
 
-from did_django_google_api_tutorial.mixins import Directions
+from safety_planning_map.mixins import Directions
 '''
 Basic view for routing 
 '''
--- safety_planning_map/asgi.py
+++ safety_planning_map/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for did_django_google_api_tutorial project.
+ASGI config for safety_planning_map project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'did_django_google_api_tutorial.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'safety_planning_map.settings')
 
 application = get_asgi_application()
